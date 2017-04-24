7535030c0e5080b33d333e731950aff3840b51ab
一波 <wuyb@jiahenghealth.com>
2017-04-24 14:57:22 +0800

    增加微信 URL Schame 跳转微信plist

---
+++ YBThirdLoginTest/AppDelegate+WeiXin.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate+WeiXin.h
+//  YBThirdLoginTest
+//
+//  Created by 一波 on 2017/4/24.
+//  Copyright © 2017年 一波. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <WXApi.h>
+
+#define kWeiXinAppKey @"wx9d4b9f65e8a2503b"
+
+@interface AppDelegate (WeiXin)<WXApiDelegate>
+
+@end
---
+++ YBThirdLoginTest/AppDelegate+WeiXin.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+WeiXin.m
+//  YBThirdLoginTest
+//
+//  Created by 一波 on 2017/4/24.
+//  Copyright © 2017年 一波. All rights reserved.
+//
+
+#import "AppDelegate+WeiXin.h"
+
+@implementation AppDelegate (WeiXin)
+
+@end
