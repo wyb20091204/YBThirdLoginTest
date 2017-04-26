51f46e7076fde00ce9bafdcb0acfb494e69807dc
一波 <wuyb@jiahenghealth.com>
2017-04-26 15:10:01 +0800

    key 保密

--- YBThirdLoginTest/AppDelegate+ThirdPartLogin.h
+++ YBThirdLoginTest/AppDelegate+ThirdPartLogin.h
@@ -9,21 +9,20 @@
 #import "AppDelegate.h"
 
 #import <WXApi.h>
-#define kWeiXinAppKey @"wx539fba18a39b78e1"
-#define kWeiXinAppSecret @"eed6f0dc893c750c5d1022c2d2c0916d"
+#define kWeiXinAppKey @"kWeiXinAppKey"
+#define kWeiXinAppSecret @"kWeiXinAppSecret"
 
 #import <TencentOpenAPI/TencentOAuth.h>
-#define kQQAppID @"1106118074"
+#define kQQAppID @"kQQAppID"
 
 #import <WeiboSDK.h>
-#define kWeiBoAppID @"65135555"
-#define kWeiBoAppKey @"2796573326"
-#define kWeiBoAppSecret @"03897e3fac94addbb10668ad5bd56212"
+#define kWeiBoAppID @"kWeiBoAppID"
+#define kWeiBoAppKey @"kWeiBoAppKey"
+#define kWeiBoAppSecret @"kWeiBoAppSecret"
 #define kWeiBoRedirectURI  @"https://api.weibo.com/oauth2/default.html"
 
 
 @interface AppDelegate (ThirdPartLogin)<WXApiDelegate,WeiboSDKDelegate>
-
 
 - (void)registToWeChat;
 - (void)registToWeiBo;
